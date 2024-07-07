ff6f610eb00b476c069e448883b3dd9b448f2de9
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-07-07 14:40:43 +0300

    add all the required methods

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
@@ -16,13 +17,32 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
-    public  void  draw(){
+    public  void  draw(Graphics g){
 
     }
 
+    public  void checkApple(){
+
+    }
+
+    public  void checkCollisions(){
+
+    }
+
+    public  void  gameOver(Graphics g){
+
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public  class MyKeyAdapter extends KeyAdapter {
+        @Override
+
+        public  void keyPressed(KeyEvent e){
+
+        }
     }
 }
