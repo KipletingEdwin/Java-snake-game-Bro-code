a4ad7d26e80628ca0bdd95dc61bce9e2f8722d5f
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-07-10 20:44:34 +0300

    add grid lines

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -27,18 +27,36 @@ public class GamePanel extends JPanel implements ActionListener {
         this.setBackground(Color.black);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
+        startGame();
 
     }
 
     public void  startGame(){
-
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
 
-    public  void  paintComponent(){
+    public  void  paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
 
     }
 
     public  void  draw(Graphics g){
+        for(int i = 0; i<SCREEN_HEIGHT/UNIT_SIZE;i++){
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        }
+
+    }
+
+    public  void newApple(){
+
+    }
+
+    public  void  move(){
 
     }
 
